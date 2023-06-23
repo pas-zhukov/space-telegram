72dc404364e4a1135d95d39d04c6f2d62200cb2c
Pavel <pas-zhukov@yandex.ru>
2023-06-23 22:31:37 +0300

    add globals default values

--- global_vars_env.py
+++ global_vars_env.py
@@ -6,15 +6,15 @@ from dotenv import load_dotenv
 load_dotenv()
 
 
-IMAGES_PATH = os.getenv("IMAGES_PATH")
+IMAGES_PATH = os.getenv("IMAGES_PATH", None)
 pathlib.Path(f"{IMAGES_PATH}/").mkdir(parents=True, exist_ok=True)
 
 
-NASA_API_KEY = os.getenv("NASA_API_KEY")
+NASA_API_KEY = os.getenv("NASA_API_KEY", None)
 
 
-TG_BOT_TOKEN = os.getenv("TG_BOT_TOKEN")
-TG_CHANNEL_NAME = os.getenv("TG_CHANNEL_NAME")
+TG_BOT_TOKEN = os.getenv("TG_BOT_TOKEN", None)
+TG_CHANNEL_NAME = os.getenv("TG_CHANNEL_NAME", None)
 
 
 SPACEX_API_METHOD_URL = 'https://api.spacexdata.com/v5/launches'
